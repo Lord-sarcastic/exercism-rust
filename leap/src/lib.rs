594b9ddc59b5ef938b0169033c1aa04299fc74a5
Lord-sarcastic <adeoti.15.jude@gmail.com>
2020-06-20 02:00:18 +0100

    Chore: Reworked leap year solution

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,13 +1,7 @@
 pub fn is_leap_year(year: u64) -> bool {
     if year % 100 == 0 {
-        if year % 400 == 0 {
-            return true;
-        } else {
-            return false;
-        }
-    } else if year % 4 == 0 {
-        return true;
+        return year % 400 == 0;
     }
 
-    false
+    year % 4 == 0
 }
